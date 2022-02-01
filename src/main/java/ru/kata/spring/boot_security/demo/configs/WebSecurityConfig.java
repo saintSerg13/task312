9d8a354311aab8ec8ba1f88300ae3841db1fab29
one of <pozdn-olesya@yandex.ru>
2022-02-01 18:05:34 +0300

    final

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -27,7 +27,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/user").hasAnyRole("USER")
                 .and()
                     .formLogin()
                     .successHandler(successUserHandler)
